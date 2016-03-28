470f9f01fa17dc121116574deffd1a1023d81f8f
collenirwin <collenirwin@gmail.com>
2016-03-28 00:27:34 -0400

    minor fix - ignore

--- post/index.php
+++ post/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
 <head>
@@ -72,3 +73,29 @@
     </div>
 </body>
 </html>
+=======
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Write - PostTitleHere</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="content">
+            <div id="title">
+                <?php
+                echo $_POST["title"];
+                ?>
+            </div>
+            <div id="body">
+                <?php
+                echo $_POST["body"];
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+>>>>>>> parent of d8c9204... Added a practice directory with 2 small C# projects I created this week. I will add more practice projects to this directory in the future.
